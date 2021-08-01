fdef56ae218a9c03b20038992268469b10c4f771
abszerro <iii554_i@mai.ru>
2021-08-02 00:17:41 +0300

    Button

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.metrics import dp
 from kivy.uix.boxlayout import BoxLayout
-from kivy.core.window import Window
 from random import randint
-
-Window.size = (400,400)
-Window.clearcolor = (250/255, 194/255, 153/255, 1)
-Window.title = "Приложение"
 
 class MyApp(App):
     def __init__(self):
